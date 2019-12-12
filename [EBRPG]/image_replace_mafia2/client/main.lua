76dcf17e0994bc944bed5ad22d029d88bb83643b
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2019-12-13 02:39:13 +0600

    +27 объектов

---
+++ [EBRPG]/image_replace_mafia2/client/main.lua
@@ -0,0 +1,29 @@
+local car = {
+	"vehiclegrunge256",
+	"plateback1",
+	"plateback2",
+	"plateback3",
+	"vehiclepoldecals128",
+}
+
+function replaceTexture(textureName, imgPath)
+	local textureReplaceShader = dxCreateShader("client/shaders/texture_replace.fx", 0, 0, false, "world")
+	local texture = dxCreateTexture(imgPath .. textureName .. ".png")
+	dxSetShaderValue(textureReplaceShader, "gTexture", texture)
+	engineApplyShaderToWorldTexture(textureReplaceShader, textureName)
+end
+
+function replaceTexture2(textureName, imgPath)
+	local textureReplaceShader = dxCreateShader("client/shaders/texture_replace.fx", 150, 0, false, "vehicle")
+	local texture = dxCreateTexture(imgPath .. textureName .. ".png")
+	dxSetShaderValue(textureReplaceShader, "gTexture", texture)
+	engineApplyShaderToWorldTexture(textureReplaceShader, textureName)
+end
+
+function replaceTextures()
+	for i, textureName in ipairs(car) do
+		replaceTexture2(textureName, "client/img/")
+	end
+end
+
+addEventHandler("onClientResourceStart", resourceRoot, replaceTextures)
