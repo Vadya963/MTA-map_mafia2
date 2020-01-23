0671acce607cf1d48d2280061f06e6bfdc04a2e5
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2020-01-23 21:06:32 +0600

    фикс
    добавил полоску хп и брони

--- [EBRPG]/hud_mafia2/c_hud_mask.lua
+++ [EBRPG]/hud_mafia2/c_hud_mask.lua
@@ -34,6 +34,21 @@ function getSpeed(vehicle)
 	end
 end
 
+function getPedMaxHealth(ped)
+	-- Output an error and stop executing the function if the argument is not valid
+	assert(isElement(ped) and (getElementType(ped) == "ped" or getElementType(ped) == "player"), "Bad argument @ 'getPedMaxHealth' [Expected ped/player at argument 1, got " .. tostring(ped) .. "]")
+
+	-- Grab his player health stat.
+	local stat = getPedStat(ped, 24)
+
+	-- Do a linear interpolation to get how many health a ped can have.
+	-- Assumes: 100 health = 569 stat, 200 health = 1000 stat.
+	local maxhealth = 100 + (stat - 569) / 4.31
+
+	-- Return the max health. Make sure it can't be below 1
+	return math.max(1, maxhealth)
+end
+
 -----------------------------------------------------------------------------------
 -- onClientRender
 -----------------------------------------------------------------------------------
@@ -96,11 +111,12 @@ addEventHandler( "onClientRender", root,
 		--
 		-- Draw
 		--
-		dxDrawImage( screenWidth-146, screenHeight-161, 146, 146, hudMaskShader, 0,0,0, tocolor(255,255,255,255) )
-		--dxDrawImage( screenWidth-136, screenHeight-151, 126, 126, hudMaskShader, 0,0,0, tocolor(255,255,255,255) )
-		dxDrawImage( screenWidth-(146/2)-8, screenHeight-(146/2)-8-15, 16, 16, "images/blips/2.png", -playerrot+camrot,0,0, tocolor(255,255,255,255) )
 		dxDrawImage( screenWidth-146, screenHeight-161, 146, 146, "images/radar_ver1.png", 0,0,0, tocolor(255,255,255,255) )
-		--dxDrawImage( screenWidth-146, screenHeight-161, 146, 146, "images/radar_ver2.png", 0,0,0, tocolor(255,255,255,255) )
+		dxDrawCircle ( (screenWidth-146)+(146/2), (screenHeight-161)+(146/2), 72, 83.0, (-166.0/getPedMaxHealth(localPlayer))*getElementHealth(localPlayer)+83, tocolor( 78,135,95,255 ), tocolor( 78,135,95,255 ) )
+		dxDrawCircle ( (screenWidth-146)+(146/2), (screenHeight-161)+(146/2), 72, 97.0, (166.0/100)*getPedArmor(localPlayer)+97, tocolor( 0, 102, 255,255 ), tocolor( 0, 102, 255,255 ) )
+		dxDrawImage( screenWidth-136, screenHeight-151, 126, 126, hudMaskShader, 0,0,0, tocolor(255,255,255,255) )
+		dxDrawImage( screenWidth-(146/2)-8, screenHeight-(146/2)-8-15, 16, 16, "images/blips/2.png", -playerrot+camrot,0,0, tocolor(255,255,255,255) )
+		dxDrawImage( screenWidth-146, screenHeight-161, 146, 146, "images/radar_ver2.png", 0,0,0, tocolor(255,255,255,255) )
 		dxDrawImage( screenWidth-146, screenHeight-161, 146, 146, "images/pointer.png", camrot,0,0, tocolor(255,255,255,255) )
 	end
 )
