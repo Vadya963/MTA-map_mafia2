92a2f910808f7622200103f4a63ead17a55ec548
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2020-03-23 20:06:35 +0600

    Update theft_car_m2_c.lua

--- [EBRPG]/hud_mafia2/theft_car_m2_c.lua
+++ [EBRPG]/hud_mafia2/theft_car_m2_c.lua
@@ -12,32 +12,32 @@ local green = tocolor( 0,255,0 )
 local screenWidth, screenHeight = guiGetScreenSize ( )
 local theft = false
 local key_state = 1
-local key_ygol = -9
+local key_angle = -9
 local count = 0
 local coef_ygol = 18/28
 local timer_key_p = false
 local timer_key_m = false
 local key_table = {
-	[1] = {["pryjina"] = 0, ["stvol2"] = 0, ["stvol1"] = 0, color = white, value = false, randomize = random(1, 28)},
-	[2] = {["pryjina"] = 0, ["stvol2"] = 0, ["stvol1"] = 0, color = white, value = false, randomize = random(1, 28)},
-	[3] = {["pryjina"] = 0, ["stvol2"] = 0, ["stvol1"] = 0, color = white, value = false, randomize = random(1, 28)},
+	[1] = {["spring"] = 0, ["kernel2"] = 0, ["kernel1"] = 0, color = white, value = false, randomize = random(1, 28)},
+	[2] = {["spring"] = 0, ["kernel2"] = 0, ["kernel1"] = 0, color = white, value = false, randomize = random(1, 28)},
+	[3] = {["spring"] = 0, ["kernel2"] = 0, ["kernel1"] = 0, color = white, value = false, randomize = random(1, 28)},
 }
 local theft_img = {
-	["zamok"] = {107,0, 80,186},
-	["otv"] = {189,21, 19,83},
+	["castle"] = {107,0, 80,186},
+	["hole"] = {189,21, 19,83},
 	["key"] = {208,0, 32,187},
-	["stvol1"] = {294,465, 25,12},
-	["stvol2"] = {319,465, 25,12},
-	["pryjina"] = {368,493, 11,59},
+	["kernel1"] = {294,465, 25,12},
+	["kernel2"] = {319,465, 25,12},
+	["spring"] = {368,493, 11,59},
 }
 
 --входит в ствол на 14 пикс+14
 --пространтсво между стволами 8 пикс
 
 local theft_t = {
-	[1] = { ["otv"] = {22+19*0,21}, ["pryjina"] = {26+12*0,25, 11,40}, ["stvol2"] = {19+12*0,72}, ["stvol1"] = {19+12*0,97}, ["key"] = {-76+19*0,22} },
-	[2] = { ["otv"] = {22+19*1,21}, ["pryjina"] = {33+12*1,25, 11,40}, ["stvol2"] = {26+12*1,72}, ["stvol1"] = {26+12*1,97}, ["key"] = {-76+19*1,22} },
-	[3] = { ["otv"] = {22+19*2,21}, ["pryjina"] = {40+12*2,25, 11,40}, ["stvol2"] = {33+12*2,72}, ["stvol1"] = {33+12*2,97}, ["key"] = {-76+19*2,22} },
+	[1] = { ["hole"] = {22+19*0,21}, ["spring"] = {26+12*0,25, 11,40}, ["kernel2"] = {19+12*0,72}, ["kernel1"] = {19+12*0,97}, ["key"] = {-76+19*0,22} },
+	[2] = { ["hole"] = {22+19*1,21}, ["spring"] = {33+12*1,25, 11,40}, ["kernel2"] = {26+12*1,72}, ["kernel1"] = {26+12*1,97}, ["key"] = {-76+19*1,22} },
+	[3] = { ["hole"] = {22+19*2,21}, ["spring"] = {40+12*2,25, 11,40}, ["kernel2"] = {33+12*2,72}, ["kernel1"] = {33+12*2,97}, ["key"] = {-76+19*2,22} },
 }
 
 addEventHandler( "onClientResourceStart", resourceRoot,
@@ -58,12 +58,12 @@ function f_fun ( key, keyState )
 
 		key_state = 1
 		count = 0
-		key_ygol = -9
+		key_angle = -9
 
 		for i=1,3 do
-			key_table[i]["stvol2"] = 0
-			key_table[i]["stvol1"] = 0
-			key_table[i]["pryjina"] = 0
+			key_table[i]["kernel2"] = 0
+			key_table[i]["kernel1"] = 0
+			key_table[i]["spring"] = 0
 			key_table[i].color = white
 			key_table[i].value = false
 			key_table[i].randomize = random(1, 28)
@@ -80,10 +80,10 @@ function s_fun ( key, keyState )
 		timer_key_p = setTimer( function ( ... )
 			if 0 <= count and count <= 27 then
 				count=count+1
-				key_ygol = -9+count*coef_ygol
-				key_table[key_state]["stvol2"] = count
-				key_table[key_state]["stvol1"] = count
-				key_table[key_state]["pryjina"] = count
+				key_angle = -9+count*coef_ygol
+				key_table[key_state]["kernel2"] = count
+				key_table[key_state]["kernel1"] = count
+				key_table[key_state]["spring"] = count
 				print(count , key_table[key_state].randomize)
 				if count == key_table[key_state].randomize then
 					key_table[key_state].value = true
@@ -95,7 +95,7 @@ function s_fun ( key, keyState )
 			else
 				killTimer( timer_key_p )
 			end
-			print(key_ygol,count)
+			print(key_angle,count)
 		end, 250, 0 )
 	else
 		if isTimer( timer_key_p ) then killTimer( timer_key_p ) end
@@ -103,10 +103,10 @@ function s_fun ( key, keyState )
 		timer_key_m = setTimer( function ( ... )
 			if 1 <= count and count <= 28 then
 				count=count-1
-				key_ygol = -9+count*coef_ygol
-				key_table[key_state]["stvol2"] = count
-				key_table[key_state]["stvol1"] = count
-				key_table[key_state]["pryjina"] = count
+				key_angle = -9+count*coef_ygol
+				key_table[key_state]["kernel2"] = count
+				key_table[key_state]["kernel1"] = count
+				key_table[key_state]["spring"] = count
 				print(count , key_table[key_state].randomize)
 				if count == key_table[key_state].randomize then
 					key_table[key_state].value = true
@@ -118,7 +118,7 @@ function s_fun ( key, keyState )
 			else
 				killTimer( timer_key_m )
 			end
-			print(key_ygol,count)
+			print(key_angle,count)
 		end, 250, 0 )
 	end
 end
@@ -130,23 +130,23 @@ function e_fun ( key, keyState )
 		if isTimer( timer_key_p ) then killTimer( timer_key_p ) end
 		if isTimer( timer_key_m ) then killTimer( timer_key_m ) end
 
-		key_table[key_state]["stvol2"] = 14+8
-		key_table[key_state]["stvol1"] = 14
-		key_table[key_state]["pryjina"] = 14+8
+		key_table[key_state]["kernel2"] = 14+8
+		key_table[key_state]["kernel1"] = 14
+		key_table[key_state]["spring"] = 14+8
 		key_table[key_state].color = green
 		key_table[key_state].value = true
 
 		key_state = key_state+1
 		count = 0
-		key_ygol = -9
+		key_angle = -9
 
 	elseif key_table[key_state].value == false and 2 <= key_state and key_state <= 3 then
 		if isTimer( timer_key_p ) then killTimer( timer_key_p ) end
 		if isTimer( timer_key_m ) then killTimer( timer_key_m ) end
 
-		key_table[key_state]["stvol2"] = 0
-		key_table[key_state]["stvol1"] = 0
-		key_table[key_state]["pryjina"] = 0
+		key_table[key_state]["kernel2"] = 0
+		key_table[key_state]["kernel1"] = 0
+		key_table[key_state]["spring"] = 0
 		key_table[key_state].color = white
 		key_table[key_state].value = false
 
@@ -154,11 +154,11 @@ function e_fun ( key, keyState )
 
 		key_state = key_state-1
 		count = 0
-		key_ygol = -9
+		key_angle = -9
 
-		key_table[key_state]["stvol2"] = 0
-		key_table[key_state]["stvol1"] = 0
-		key_table[key_state]["pryjina"] = 0
+		key_table[key_state]["kernel2"] = 0
+		key_table[key_state]["kernel1"] = 0
+		key_table[key_state]["spring"] = 0
 		key_table[key_state].color = white
 		key_table[key_state].value = false
 	end
@@ -185,39 +185,39 @@ end
 function createText ()
 	if not theft then return end
 
-	local x,y = (screenWidth/2)-(theft_img["zamok"][3]/2), (screenHeight/2)-(theft_img["zamok"][4]/2)
+	local x,y = (screenWidth/2)-(theft_img["castle"][3]/2), (screenHeight/2)-(theft_img["castle"][4]/2)
 
-	dxDrawImageSection( x, y, theft_img["zamok"][3], theft_img["zamok"][4], theft_img["zamok"][1], theft_img["zamok"][2], theft_img["zamok"][3], theft_img["zamok"][4], 'hud/hud2.png', 0, 0, 0, tocolor( 255,255,255,255 ) )
+	dxDrawImageSection( x, y, theft_img["castle"][3], theft_img["castle"][4], theft_img["castle"][1], theft_img["castle"][2], theft_img["castle"][3], theft_img["castle"][4], 'hud/hud2.png', 0, 0, 0, tocolor( 255,255,255,255 ) )
 	
-	local i, v = "otv", theft_t[1]["otv"]
+	local i, v = "hole", theft_t[1]["hole"]
 	dxDrawImageSection( x+v[1], y+v[2], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', 0, 0, 0, tocolor( 255,255,255,255 ) )
-	local i, v = "otv", theft_t[2]["otv"]
+	local i, v = "hole", theft_t[2]["hole"]
 	dxDrawImageSection( x+v[1], y+v[2], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', 0, 0, 0, tocolor( 255,255,255,255 ) )
-	local i, v = "otv", theft_t[3]["otv"]
+	local i, v = "hole", theft_t[3]["hole"]
 	dxDrawImageSection( x+v[1], y+v[2], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', 0, 0, 0, tocolor( 255,255,255,255 ) )
 
-	local i, v = "stvol2", theft_t[1]["stvol2"]
+	local i, v = "kernel2", theft_t[1]["kernel2"]
 	dxDrawImageSection( x+v[1], y+v[2]-key_table[1][i], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', -90, 0, 0, key_table[1].color )
-	local i, v = "stvol2", theft_t[2]["stvol2"]
+	local i, v = "kernel2", theft_t[2]["kernel2"]
 	dxDrawImageSection( x+v[1], y+v[2]-key_table[2][i], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', -90, 0, 0, key_table[2].color )
-	local i, v = "stvol2", theft_t[3]["stvol2"]
+	local i, v = "kernel2", theft_t[3]["kernel2"]
 	dxDrawImageSection( x+v[1], y+v[2]-key_table[3][i], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', -90, 0, 0, key_table[3].color )
 
-	local i, v = "stvol1", theft_t[1]["stvol1"]
+	local i, v = "kernel1", theft_t[1]["kernel1"]
 	dxDrawImageSection( x+v[1], y+v[2]-key_table[1][i], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', -90, 0, 0, key_table[1].color )
-	local i, v = "stvol1", theft_t[2]["stvol1"]
+	local i, v = "kernel1", theft_t[2]["kernel1"]
 	dxDrawImageSection( x+v[1], y+v[2]-key_table[2][i], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', -90, 0, 0, key_table[2].color )
-	local i, v = "stvol1", theft_t[3]["stvol1"]
+	local i, v = "kernel1", theft_t[3]["kernel1"]
 	dxDrawImageSection( x+v[1], y+v[2]-key_table[3][i], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', -90, 0, 0, key_table[3].color )
 
-	local i, v = "pryjina", theft_t[1]["pryjina"]
+	local i, v = "spring", theft_t[1]["spring"]
 	dxDrawImageSection( x+v[1], y+v[2], v[3], v[4]-key_table[1][i], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', 0, 0, 0, tocolor( 255,255,255,255 ) )
-	local i, v = "pryjina", theft_t[2]["pryjina"]
+	local i, v = "spring", theft_t[2]["spring"]
 	dxDrawImageSection( x+v[1], y+v[2], v[3], v[4]-key_table[2][i], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', 0, 0, 0, tocolor( 255,255,255,255 ) )
-	local i, v = "pryjina", theft_t[3]["pryjina"]
+	local i, v = "spring", theft_t[3]["spring"]
 	dxDrawImageSection( x+v[1], y+v[2], v[3], v[4]-key_table[3][i], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', 0, 0, 0, tocolor( 255,255,255,255 ) )
 
 	local i, v = "key", theft_t[key_state]["key"]
-	dxDrawImageSection( x+v[1], y+v[2], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', 90-key_ygol, 0, 0, tocolor( 255,255,255,255 ) )
+	dxDrawImageSection( x+v[1], y+v[2], theft_img[i][3], theft_img[i][4], theft_img[i][1], theft_img[i][2], theft_img[i][3], theft_img[i][4], 'hud/hud2.png', 90-key_angle, 0, 0, tocolor( 255,255,255,255 ) )
 end
 addEventHandler ( "onClientRender", root, createText )
