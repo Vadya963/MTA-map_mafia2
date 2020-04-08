ae0d01af1ba0bf02c16c52343f86006d533c633c
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2020-04-08 23:21:38 +0600

    Update object_replace.lua

--- [EBRPG]/map_mafia2/object_replace.lua
+++ [EBRPG]/map_mafia2/object_replace.lua
@@ -1,20 +1,22 @@
-local eb_textures = {
-	["prison"] = engineLoadTXD ( ":textures/prison.txd" ),
-}
+local eb_textures = {}
 local weather = "summer" --summer,winter,autumn
 
 addEventHandler( "onClientResourceStart", resourceRoot,
 function ( startedRes )
 	for k,v in ipairs(getElementData(root, "object")) do
-		if weather == "summer" and not eb_textures[v[4]] then
+		if weather == "summer" and not eb_textures[v[4]] and fileExists(":textures/"..v[4].."_summer.txd") then
 			eb_textures[v[4]] = engineLoadTXD ( ":textures/"..v[4].."_summer.txd" )
 			print(v[4].." load")
-		elseif weather == "winter" and not eb_textures[v[4]] then
+		elseif weather == "winter" and not eb_textures[v[4]] and fileExists(":textures/"..v[4].."_winter.txd") then
 			eb_textures[v[4]] = engineLoadTXD ( ":textures/"..v[4].."_winter.txd" )
 			print(v[4].." load")
 		elseif weather == "autumn" and not eb_textures[v[4]] then
 			--soon
-			--eb_textures[v[4]] = engineLoadTXD ( ":textures/"..v[4].."_autumn.txd" )
+		end
+
+		if not eb_textures[v[4]] and fileExists(":textures/"..v[4]..".txd") then
+			eb_textures[v[4]] = engineLoadTXD ( ":textures/"..v[4]..".txd" )
+			print(v[4].." load")
 		end
 
 		if fileExists(":map_mafia2/"..v[4].."/"..v[1]..".dff") and fileExists(":map_mafia2/"..v[4].."/"..v[1]..".col") then
