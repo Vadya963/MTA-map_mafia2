4a2ca98fd94f350df5f8467bd2810062dbe98619
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2020-07-06 14:44:08 +0600

    оставил только карту

--- [EBRPG]/hud_mafia2/settings_c.lua
+++
@@ -1,2 +0,0 @@
-local screenWidth, screenHeight = guiGetScreenSize ( )
-width_hd, height_hd = (screenWidth/1280),(screenHeight/720)--адаптация размеров
--- [EBRPG]/image_replace_mafia2/client/shaders/texture_replace.fx
+++
@@ -1,7 +0,0 @@
-texture gTexture;
-
-technique TextureReplace {
-	pass Pass0 {
-		Texture[0] = gTexture;
-	}
-}
