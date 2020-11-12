084bf0d944a7182d02ffd16a1cd7f6c834d2e2e5
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2020-11-13 04:27:28 +0600

    upd
    оптимизация текстур

--- [EBRPG]/map_mafia2/object_replace.lua
+++ [EBRPG]/map_mafia2/object_replace.lua
@@ -3,12 +3,11 @@ local weather = "summer" --summer,winter
 
 addEventHandler( "onClientResourceStart", resourceRoot,
 function ( startedRes )
+	eb_textures["ground"] = engineLoadTXD ( ":map_mafia2/textures/ground_"..weather..".txd" )
+
 	for k,v in ipairs(getElementData(root, "object_replace")) do
-		if weather == "summer" and not eb_textures[v[3]] and fileExists(":map_mafia2/textures/"..v[3].."_summer.txd") then
-			eb_textures[v[3]] = engineLoadTXD ( ":map_mafia2/textures/"..v[3].."_summer.txd" )
-			print(v[3].." load")
-		elseif weather == "winter" and not eb_textures[v[3]] and fileExists(":map_mafia2/textures/"..v[3].."_winter.txd") then
-			eb_textures[v[3]] = engineLoadTXD ( ":map_mafia2/textures/"..v[3].."_winter.txd" )
+		if not eb_textures[v[3]] and fileExists(":map_mafia2/textures/"..v[3].."_"..weather..".txd") then
+			eb_textures[v[3]] = engineLoadTXD ( ":map_mafia2/textures/"..v[3].."_"..weather..".txd" )
 			print(v[3].." load")
 		end
 
@@ -19,15 +18,23 @@ function ( startedRes )
 
 		if fileExists(":map_mafia2/"..v[3].."/"..v[1]..".dff") and fileExists(":map_mafia2/"..v[3].."/"..v[1]..".col") then
 			engineImportTXD (eb_textures[v[3]], v[2])
+
+			if fileExists(":map_mafia2/textures/"..v[3].."_"..weather..".txd") then
+				engineImportTXD (eb_textures["ground"], v[2])
+			end
+
 			local dff = engineLoadDFF ( ":map_mafia2/"..v[3].."/"..v[1]..".dff" )
 			if v[4] then
 				engineReplaceModel ( dff, v[2], true )
 			else
 				engineReplaceModel ( dff, v[2], false )
 			end
+
 			local col = engineLoadCOL ( ":map_mafia2/"..v[3].."/"..v[1]..".col" )
 			engineReplaceCOL ( col, v[2] )
+
 			engineSetModelLODDistance(v[2], 30000)
+
 			if v[5] then
 				engineSetModelPhysicalPropertiesGroup(v[2], v[5])
 			else
