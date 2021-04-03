2579e517f23b6f86d1930cab5849e528ccff3ac5
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2021-04-04 00:56:09 +0600

    upd
    добавил фонарный столб

--- [EBRPG]/map_mafia2_pos/water_c.lua
+++ [EBRPG]/map_mafia2_pos/water_c.lua
@@ -53,7 +53,7 @@ function ( startedRes )
 
 	for k,v in pairs(getElementData(root, "object")) do
 		local obj = getLowLODElement(v[2])
-		if v[5] then
+		if v[5] == true then--в city_crash это название интерьеров, нужна проверка
 			setElementDoubleSided(v[2], true)
 			if obj then setElementDoubleSided(obj, true) end
 		else
