2e559ad23170ef702dfee509110c88b0aae13aea
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2021-05-23 21:38:30 +0600

    upd hillwood

--- [EBRPG]/programm from export/object_collapse_3ds.lua
+++ [EBRPG]/programm from export/object_collapse_3ds.lua
@@ -1,10 +1,17 @@
-local value = true--true matrix, false euler angles
+local value = nil--true matrix, false euler angles, nil other
 local object_collapse = {
-{'3PO_wood_door', {{1 ,0 ,0 ,0}, {0 ,1 ,0 ,0}, {0 ,0 ,1 ,0}, {-422.4453 ,778.3784 ,-17.83264 ,0},}, 0},
-{'3PO_wood_door04', {{5.960465E-08 ,1 ,0 ,0}, {-1 ,5.960465E-08 ,0 ,0}, {0 ,0 ,1 ,0}, {-444.3612 ,742.7476 ,-17.83264 ,0},}, 0},
-{'3PO_wood_door01', {{1 ,0 ,0 ,0}, {0 ,1 ,0 ,0}, {0 ,0 ,1 ,0}, {-444.1415 ,778.3874 ,-17.83264 ,0},}, 0},
-{'3PO_wood_door02', {{-1 ,-1.509958E-07 ,0 ,0}, {1.509958E-07 ,-1 ,0 ,0}, {0 ,0 ,1 ,0}, {-438.6436 ,741.0653 ,-17.83264 ,0},}, 0},
-{'3PO_wood_door03', {{3.576279E-07 ,-1 ,0 ,0}, {1 ,3.576279E-07 ,0 ,0}, {0 ,0 ,1 ,0}, {-422.2255 ,746.8673 ,-17.83264 ,0},}, 0},
+{'14_plot_svah__04', {{-0.08671875 ,1303.536 ,55.61641 ,0 ,0 ,0},
+{21.7834 ,1303.536 ,55.61641 ,0 ,0 ,0},
+{-0.08671875 ,1280.721 ,61.70739 ,0 ,0 ,0},
+{21.7834 ,1280.721 ,61.70739 ,0 ,0 ,0},
+{150.941 ,1312.585 ,52.73891 ,0 ,0 ,0},
+{168.5385 ,1312.585 ,52.73891 ,0 ,0 ,0},
+{189.0309 ,1314.27 ,52.70752 ,0 ,0 ,0},
+{206.6284 ,1314.27 ,52.70752 ,0 ,0 ,0},
+{225.9795 ,1315.688 ,53.40806 ,0 ,0 ,0},
+{243.577 ,1315.688 ,53.40806 ,0 ,0 ,0},
+{263.3451 ,1314.679 ,55.63765 ,0 ,0 ,0},
+{280.9426 ,1314.679 ,55.63765 ,0 ,0 ,0}}, 0},
 }
 
 function getEulerAnglesFromMatrix(table)
@@ -43,16 +50,23 @@ local file_new = io.open(pyt_new, "w")
 file_new:write("inst\n")
 
 for k,v in pairs(object_collapse) do
-	if value then
+	if value == true then
 		local x,y,z = v[2][4][1],v[2][4][2],v[2][4][3]
 		local xr,yr,zr = getEulerAnglesFromMatrix(v)
 		local rot = toQuaternion(xr,yr,zr)-- zr*-1 from mafiatoolkit ver 2.13
 		file_new:write(v[3]..", "..name..", 0, "..x-object_collapse[1][2][4][1]..", "..y-object_collapse[1][2][4][2]..", "..z-object_collapse[1][2][4][3]..", "..rot.x..", "..rot.y..", "..rot.z..", "..rot.w..", -1\n" )
-	else
+	elseif value == false then
 		local xr,yr,zr = v[2][4],v[2][5],v[2][6]
 		local x,y,z = v[2][1],v[2][2],v[2][3]
 		local rot = toQuaternion(xr,yr,zr)
 		file_new:write(v[3]..", "..name..", 0, "..x-object_collapse[1][2][1]..", "..y-object_collapse[1][2][2]..", "..z-object_collapse[1][2][3]..", "..rot.x..", "..rot.y..", "..rot.z..", "..rot.w..", -1\n" )
+	elseif value == nil then
+		for k,v in pairs(object_collapse[1][2]) do
+			local xr,yr,zr = v[4],v[5],v[6]
+			local x,y,z = v[1],v[2],v[3]
+			local rot = toQuaternion(xr,yr,zr)
+			file_new:write(object_collapse[1][3]..", "..name..", 0, "..x-object_collapse[1][2][1][1]..", "..y-object_collapse[1][2][1][2]..", "..z-object_collapse[1][2][1][3]..", "..rot.x..", "..rot.y..", "..rot.z..", "..rot.w..", -1\n" )
+		end
 	end
 end
 
