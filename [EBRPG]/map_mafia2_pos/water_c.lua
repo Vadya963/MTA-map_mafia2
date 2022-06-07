fc06c6d3473f54a5772d91fe0d5b2dc3031f31d0
Vadya963 <38101660+Vadya963@users.noreply.github.com>
2022-06-08 03:09:07 +0600

    Update water_c.lua

--- [EBRPG]/map_mafia2_pos/water_c.lua
+++ [EBRPG]/map_mafia2_pos/water_c.lua
@@ -4,13 +4,42 @@ local table_water = {
 	[3] = {-379.68450927734,1274.3002929688, 1007.826171875,1530.5191650391, {false, false}},--от дип до дамбы
 	[4] = {-83.8125,1530.5191650391, 1007.826171875,1931.6591796875, {false, false}},--от ривер до дамбы
 	[5] = {-1264.7642822266,194.21995544434, -718.77734375,349.09210205078, {false, false}},--от хант до хант
-	[6] = {-1340.3854980469,-412.2067565918, -718.77734375,194.21995544434, {false, false}},--от хант до саус --нужная саус
-	[7] = {-1336.3854980469,-2000, -974.7460937,-412.2067565918, {false, false}},--от саус до саус --нужная саус
+	[6] = {-1340.3854980469,-412.2067565918, -718.77734375,194.21995544434, {false, false}},--от хант до саус
+	[7] = {-1336.3854980469,-2000, -974.7460937,-412.2067565918, {false, false}},--от саус до саус
 	[8] = {-718.77734375,1263.2564697266, -631.71551513672,1365.2574462891, {false, false}},--от кинг до кинг
 	[9] = {-974.7460937,-2000, 1500.31118774414,-503.91384887695, {false, false}},--от саус до юж мил
 	[10] = {813.47155761719,-503.91384887695, 1500.31118774414,-301.46527099609, {false, false}},--от юж мил до юж мил
 }
 local height = 15
+
+local water_in_tunnel = {
+	{createColCuboid( -876, 990, 0, 386, 400, 32 ), 2,8},
+	{createColCuboid( -1400, -490, 4, 482, 213, 15 ), 6,7}
+}
+
+for k,v in pairs(water_in_tunnel) do
+	addEventHandler( "onClientColShapeHit", v[1], 
+	function (hitElement, matchingDimension) 
+	--The source of this event is the colshape that got hit by a player or vehicle.
+		if (getElementType( hitElement ) == "vehicle" and getVehicleType( hitElement ) == "Automobile") or getElementType( hitElement ) == "player" then 
+			setElementDimension( table_water[v[2]][5][1], 9999 )
+			setElementDimension( table_water[v[2]][5][2], 9999 )
+			setElementDimension( table_water[v[3]][5][1], 9999 )
+			setElementDimension( table_water[v[3]][5][2], 9999 )
+		end
+	end)
+
+	addEventHandler( "onClientColShapeLeave", v[1], 
+	function (leaveElement, matchingDimension) 
+	--The source of this event is the colshape that the element no longer is in contact with.
+		if (getElementType( leaveElement ) == "vehicle" and getVehicleType( leaveElement ) == "Automobile") or getElementType( leaveElement ) == "player" then 
+			setElementDimension( table_water[v[2]][5][1], 0 )
+			setElementDimension( table_water[v[2]][5][2], 0 )
+			setElementDimension( table_water[v[3]][5][1], 0 )
+			setElementDimension( table_water[v[3]][5][2], 0 )
+		end
+	end)
+end
 
 local swim_time, air_time = 0, 0
 setTimer(function ( ... )
@@ -45,6 +74,8 @@ function ( startedRes )
 	setBirdsEnabled(false)
 	setCloudsEnabled(false)
 	--setFogDistance(2000)
+	setDevelopmentMode( true )
+
 
 	for k,v in ipairs(table_water) do
 		v[5][1] = createWater ( v[1], v[2], height, v[3], v[2], height, v[1], v[4], height, v[3], v[4], height )
